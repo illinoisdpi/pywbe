ecb8dcbfa80ce07d882ebd7811931d92e431e171
raj1401 <rajmehta1401@gmail.com>
2024-01-07 23:47:37 -0600

    Added testing of perc_conc_change function
    Added testing of calculate_weekly_concentration_perc_change function and tweaked docstrings of plot_time_series function.

--- src/pyWBE/preliminary_functions.py
+++ src/pyWBE/preliminary_functions.py
@@ -11,23 +11,26 @@ Note: Add type-hints and docstrings to functions as they are implemented.
 
 
 from pyWBE.exceptions import FunctionNotImplementedError
+from typing import Union
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
 
-def plot_time_series(series_x: pd.Series, series_y: pd.Series, plot_type: str = "linear") -> None:
+def plot_time_series(series_x: pd.Series, series_y: pd.Series[Union[float, int]], plot_type: str = "linear") -> None:
     """
     This function plots the given time-series data for easy visualization.
 
     :param series_x: The independent variable, usually indicating time
     steps in arbitrary or specific units
-    :type series_x: pd.Series
+    :type series_x: Pandas Series
     :param series_y: The dependent variable, indicating values of the
     variable of interest over time
-    :type series_y: pd.Series
+    :type series_y: Pandas Series (of type float or int)
     :param plot_type: Determines the type of plot that is plotted.
-    It can be either 'linear' (default) or 'log'
+    It can be either 'linear' (default) or 'log'. 'linear' plots
+    series_y v/s series_x, 'log' plots the natural log of
+    series_y v/s series_x
     :type plot_type: str
     """
 
@@ -47,23 +50,22 @@ def plot_time_series(series_x: pd.Series, series_y: pd.Series, plot_type: str = 
         raise ValueError(f"The 'plot_type' parameter can only be 'linear' or 'log'. {plot_type} is invalid.")
 
 
-def calculate_weekly_concentration_perc_change(conc_data: pd.Series) -> pd.Series:
+def calculate_weekly_concentration_perc_change(conc_data: pd.Series[Union[float, int]]) -> pd.Series:
     """
     This function computes the weekly percentage change in concentration levels
     in the given time-series data.
     :param conc_data: The concentration data, assumed to have a periodicity of 1 week
-    :type conc_data: pd.Series
+    :type conc_data: Pandas Series (of type float or int)
     :return: Returns the weekly percentage change in concentration levels
     :rtype: pd.Series
     """
 
     shifted_series = conc_data.copy(deep=True)
     shifted_series.iloc[:-1] = conc_data.iloc[1:]
-    shifted_series[-1] = conc_data[-1]
 
     perc_change = (shifted_series - conc_data) / conc_data * 100
 
-    return perc_change
+    return perc_change.iloc[:-1]
 
 
 def analyze_trends(data, analysis_type):

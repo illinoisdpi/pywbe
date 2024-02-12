69de6ae1145d44baf212a91f12a608af32a3dc5c
raj1401 <rajmehta1401@gmail.com>
2024-02-12 12:21:08 -0600

    Implemented analyze trends function

--- src/pyWBE/preliminary_functions.py
+++ src/pyWBE/preliminary_functions.py
@@ -67,10 +67,21 @@ def calculate_weekly_concentration_perc_change(conc_data: pd.Series) -> pd.Serie
     return perc_change.iloc[:-1]
 
 
-def analyze_trends(data: pd.Series, break_points: list,
-                   test_type: str = "Original", alpha: float = 0.05) -> list:
-    # Uptrend, Down trend, stationary trend, cyclic trend
-    raise FunctionNotImplementedError("""The function to analyze trends has not been implemented.""")
+def analyze_trends(data: pd.Series) -> list:
+    """
+    This function computes the trend line for the given data.\n
+    :param data: The time-series data (assumed to be sorted
+    in an increasing order of time).\n
+    :type data: pd.Series\n
+    :return: Returns the trend line values which can be plotted
+    as date v/s returned trend line values.\n
+    :rtype: list
+    """
+    z = np.polyfit(range(len(data)), data, 1)
+    p = np.poly1d(z)
+    trend_vals = p(range(len(data)))
+    trend_vals = list(trend_vals)
+    return trend_vals
 
 
 def change_point_detection(time_instance, method):
